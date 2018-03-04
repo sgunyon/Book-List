58c143fa09b227bebc94574ba6e9b4ad0892e448
sgunyon <7231693+sgunyon@users.noreply.github.com>
2018-03-03 19:20:49 -0500

    Develop (#2)
    * Changed Database class to test for existing database + syntax fixes
    
    * Removed the query class and updated Database methods.
    
    * Added search namespace
    
    * Added author-to-title matching in keyword searches
    
    * Fixed api calls for adding books from good reads
    
    * Fixed searching/adding bugs

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 from database import Database
-from api import Search
-from query import Query
+from search import search_keyword, search_booklist, get_details, book_info
+
 
 def main():
     db = Database()
+    session = db.session()
 
-    selection = input('Search Bookshelf or Library: ').lower()
+    selection = input('Search Bookshelf or Library?: ').lower()
 
     if selection == 'bookshelf':
-        print('Search Criteria: title, author, genre, rereads')
-        criteria = input('Enter search criteria: ')
-        retrieve = Query(criteria, db.return_engine())
-        session = Query(criteria, db.return_engine())
-        retrieve.search_booklist(session.start_session(db.return_engine()))
-    elif selection == 'library':
-        criteria = input('Search by author or title?: ')
-        search = Search()
+        criteria = input(
+            'Enter search criteria (title, author, genre, rereads): '
+            ).lower()
+        search_booklist(criteria, session)
 
-        if criteria == 'author':
-            search.search_by_author()
-        elif criteria == 'title':
-            search.search_by_title()
-        else:
-            print('Invalid search field')
+    elif selection == 'library':
+        title, author = search_keyword()
+        add = input(
+            'Would you like to add any of these books to your bookshelf? (yes/no) '
+            ).lower()
+        if add == 'yes':
+            selection = input(
+                'Please provide the number for the book you would like to add: '
+            )
+            index = (int(selection) - 1)
+            tree = get_details(author[index], title[index])
+            title, author, genre = book_info(tree)
+            db.commit_to_db(session, title, author, genre)
+
     else:
-        print('Invalid search method')
+        print('Invalid selection')
 
 main()

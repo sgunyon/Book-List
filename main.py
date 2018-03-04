dd6ad418580d194fc958f4477f09171c2e7b50cc
sgunyon <7231693+sgunyon@users.noreply.github.com>
2018-03-03 23:25:59 -0500

    Created interface loop and removed API key (#5)

--- main.py
+++ main.py
@@ -5,30 +5,31 @@ from search import search_keyword, search_booklist, get_details, book_info
 def main():
     db = Database()
     session = db.session()
+    selection = []
 
-    selection = input('Search Bookshelf or Library?: ').lower()
+    while selection != 'exit':
+        selection = input('Search Bookshelf or Library?: ').lower()
 
-    if selection == 'bookshelf':
-        criteria = input(
-            'Enter search criteria (title, author, genre, rereads): '
-            ).lower()
-        search_booklist(criteria, session)
-
-    elif selection == 'library':
-        title, author = search_keyword()
-        add = input(
-            'Would you like to add any of these books to your bookshelf? (yes/no) '
-            ).lower()
-        if add == 'yes':
+        if selection == 'bookshelf':
             selection = input(
-                'Please provide the number for the book you would like to add: '
-            )
-            index = (int(selection) - 1)
-            tree = get_details(author[index], title[index])
-            title, author, genre = book_info(tree)
-            db.commit_to_db(session, title, author, genre)
+                'Enter search criteria (title, author, genre, rereads): '
+                ).lower()
+            search_booklist(selection, session)
 
-    else:
-        print('Invalid selection')
+        elif selection == 'library':
+            title, author = search_keyword()
+            selection = input(
+                'Would you like to add any of these books to your bookshelf? (yes/no) '
+                ).lower()
+            if selection == 'yes':
+                selection = input(
+                    'Please provide the number for the book you would like to add: '
+                )
+                selection = (int(selection) - 1)
+                tree = get_details(author[selection], title[selection])
+                title, author, genre = book_info(tree)
+                db.commit_to_db(session, title, author, genre)
+        elif selection != 'exit':
+            print('Invalid selection')
 
 main()

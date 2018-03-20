f5b2540b70d5c8c645fbad7828aa3414ea6d16c4
jillichrome <31424610+jillichrome@users.noreply.github.com>
2018-03-19 20:47:25 -0400

    Create menu for user interface (#14)

--- bookshelf.py
+++ bookshelf.py
@@ -1,14 +1,27 @@
-from database import Book
+from database import Book, Database
 from sqlalchemy import *
+from search import search_keyword, search_details, book_info
 
-def search_booklist(session):
+db = Database()
+session = db.session()
+
+def search_booklist():
     keyword = input('Enter a search term: ')
     criteria = session.query(
         Book.id, Book.title).filter(
         Book.title.ilike('%' + keyword + '%')).all()
     [print(i[0], '-', i[1]) for i in criteria]
 
-def get_details(session, book_id):
+def search_bookshelf():
+    search_booklist()
+    selection = input('Enter a number for book details: ')
+    book_id = get_details(selection)
+    selection = input('Change read count?: ').lower()
+    if selection == 'yes':
+        selection = input('New read count: ')
+        reads(book_id, selection)
+
+def get_details(book_id):
     selection = session.query(
         Book.title, Book.author, Book.genre, Book.read_count, Book.id).filter(
             Book.id == book_id).all()
@@ -22,6 +35,16 @@ def get_details(session, book_id):
 
         return detail[4]
 
-def reads(session, book, read_count):
+def update_bookshelf():
+    title, author = search_keyword()
+    selection = input(
+        'Please provide the number for the book you would like to add: '
+    )
+    selection = (int(selection) - 1)
+    tree = search_details(author[selection], title[selection])
+    title, author, genre, read_count = book_info(tree)
+    db.commit_to_db(session, title, author, genre, read_count)
+
+def reads(book, read_count):
     book_target = session.query(Book).filter(Book.id == book).first()
     book_target.read_count = read_count
---
+++ menu.py
@@ -0,0 +1,73 @@
+import os
+import sys
+
+from search import search_keyword
+from bookshelf import search_bookshelf, update_bookshelf
+
+
+menu_action = {}
+
+def main_menu():
+    os.system('clear')
+
+    print('\n MAIN MENU')
+    print('b : Bookshelf')
+    print('l : Library')
+    print('q : Quit')
+    option = input('Select an option >> ')
+    exec_menu(option)
+
+    return
+
+def exec_menu(option):
+    os.system('clear')
+    op = option.lower()
+    if op == '':
+        menu_action['main_menu']()
+    else:
+        try:
+            menu_action[op]()
+            option = input('Select an option >> ')
+        except KeyError:
+            print('Invalid Selection!')
+            menu_action['main_menu']()
+
+def bookshelf_menu():
+    print('\n Bookshelf Menu')
+    print('s : Search')
+    print('r : Return to main menu')
+    option = input('Select an option: ')
+    exec_menu(option)
+
+    return option
+
+def library_menu():
+    print('\n Library Menu')
+    print('c : Search')
+    print('u : Update Bookshelf')
+    print('r : Return to main menu')
+    print('q : Quit')
+    option = input('Select an option: ')
+    exec_menu(option)
+
+    return option
+
+def back():
+    menu_action['menu_menu']()
+
+def exit():
+    sys.exit()
+
+menu_action = {
+    'main_menu': main_menu,
+    'b' : bookshelf_menu,
+    'l' : library_menu,
+    's' : search_bookshelf,
+    'c' : search_keyword,
+    'u' : update_bookshelf,
+    'r' : back,
+    'q' : exit
+}
+
+if __name__ == '__main__':
+    main_menu()
